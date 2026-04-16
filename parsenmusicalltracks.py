4e7ea1826c37e7ea5dc1c79b114e50b89eca9a10
Florence Böttger <florence.boettger@hpi.de>
2026-04-16 10:34:57 +0200

    add ordertune localized song names from nmusic

--- parsenmusicalltracks.py
+++ parsenmusicalltracks.py
@@ -2,14 +2,20 @@ import json
 import math
 from pathlib import Path
 
-path = Path("...")
+path = Path(__file__).resolve().parent / "data" / "Original" / "alltracks_s3.json"
+datapath = Path(__file__).resolve().parent / "data" / "Splatoon 3" / "alltracks.json"
 
 with path.open("r", encoding="utf8") as f:    
     alltracks = json.loads(''.join(f.readlines()))
+
+    alltracks_old = None
+    with datapath.open("r", encoding="utf8") as g:
+        alltracks_old = json.loads(''.join(g.readlines()))
+
     export = {
         "uuid": alltracks["id"],
-        "thumbnail": "",
-        "introduction": ""
+        "thumbnail": alltracks_old["thumbnail"],
+        "introduction": alltracks_old["introduction"]
     }
 
     songlist = []
@@ -17,21 +23,28 @@ with path.open("r", encoding="utf8") as f:
         newsong = {}
         split = track["name"].split(" / ", 1)
         newsong["name"] = split[0]
+        newsong["formattedname"] = newsong["name"]
         if len(split) > 1:
             newsong["band"] = split[1]
             newsong["formattedband"] = split[1]
-        newsong["formattedname"] = newsong["name"]
-        newsong["id"] = track["id"]
-        newsong["extend"] = track["media"]["payloadList"][0]["containsLoopableMedia"]
         milliDuration = track["media"]["payloadList"][0]["durationMillis"]
         minuteDuration = milliDuration // 60000
         secondDuration = math.ceil((milliDuration % 60000) / 1000)
         newsong["length"] = f"{minuteDuration}:{secondDuration:02d}"
-        songlist.append(newsong)
+        newsong["extend"] = track["media"]["payloadList"][0]["containsLoopableMedia"]
+        newsong["uuid"] = track["id"]
+
+        exists = False
+        for old_track in alltracks_old["songlist"]:
+            if old_track["name"] == newsong["name"] and (("band" not in old_track and "band" not in newsong) or (old_track["band"] == newsong["band"])):
+                exists = True
+                songlist.append(old_track)
+                break
+        
+        if not exists:
+            songlist.append(newsong)
         
     export["songlist"] = songlist
-
-    datapath = Path(__file__).resolve().parent / "data" / "Splatoon" / "alltracks.json"
-
-    with datapath.open("w", encoding="utf8") as g:        
+    
+    with datapath.open("w", encoding="utf8") as g: 
         g.write(json.dumps(export, indent=4, ensure_ascii=False))
